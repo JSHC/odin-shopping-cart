3dced1e7eb1c2754a5bae3ec022d68b1ad997ff5
JSHC <johan.hansson.carlberg@gmail.com>
2023-04-17 19:35:54 +0200

    Add test file for ShoppingCart component

---
+++ src/components/ShoppingCart.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import ShoppingCart from './ShoppingCart';
+
+describe('ShoppingCart component', () => {
+    it('renders', () => {
+        const { container } = render(<ShoppingCart />);
+        expect(container).toMatchSnapshot();
+    }) 
+})
