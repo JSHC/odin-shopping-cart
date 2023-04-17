1a1e1e6b10cec970e098713cc4d62c4e8985fa95
JSHC <johan.hansson.carlberg@gmail.com>
2023-04-17 19:20:03 +0200

    Add BrowserRouter and initial routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
+import Home from './components/Home';
+import ShoppingCart from './components/ShoppingCart';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />}/>
+          <Route path='shopping-cart' element={<ShoppingCart />}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
