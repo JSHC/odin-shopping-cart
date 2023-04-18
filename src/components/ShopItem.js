e0d5c1e15822f74bda9d0780a0d4ee663c9a0894
JSHC <johan.hansson.carlberg@gmail.com>
2023-04-18 13:05:29 +0200

    Add label to shop item quantity

--- src/components/ShopItem.js
+++ src/components/ShopItem.js
@@ -1,44 +1,48 @@
-import { useState } from 'react';
-import '../styles/ShopItem.css';
+import { useState } from "react";
+import "../styles/ShopItem.css";
 const ShopItem = (props) => {
     const { id, name, price } = props;
 
     const [quantity, setQuantity] = useState(0);
 
     const increaseQuantity = () => {
-        setQuantity(quantity => parseInt(quantity + 1));
-    }
+        setQuantity((quantity) => parseInt(quantity + 1));
+    };
 
     const decreaseQuantity = () => {
-        setQuantity(quantity => quantity > 0 ? parseInt(quantity - 1) : 0);
-    }
+        setQuantity((quantity) => (quantity > 0 ? parseInt(quantity - 1) : 0));
+    };
 
     const updateQuantity = (e) => {
-        if (e.target.value === '') {
-            setQuantity('');
+        if (e.target.value === "") {
+            setQuantity("");
         } else if (parseInt(e.target.value) === 0) {
             setQuantity(0);
         } else {
-            setQuantity(parseInt(e.target.value) ? parseInt(e.target.value) : 0);
+            setQuantity(
+                parseInt(e.target.value) ? parseInt(e.target.value) : 0
+            );
         }
-        
-    }
+    };
     return (
         <div className="shop-item">
             <span className="shop-item-name">{name}</span>
             <span className="shop-item-price">{price}</span>
 
             <div className="shop-item-controls">
-                <input type="text" 
-                    className='shop-item-quantity' 
-                    value={quantity} 
+                <label htmlFor="shop-item-quantity">Quantity</label>
+                <input
+                    type="text"
+                    value={quantity}
                     onChange={updateQuantity}
-                    />
+                    name="shop-item-quantity"
+                    id="shop-item-quantity"
+                />
                 <button onClick={increaseQuantity}>Inc</button>
                 <button onClick={decreaseQuantity}>Dec</button>
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default ShopItem;
