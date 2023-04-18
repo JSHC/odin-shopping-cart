b45191d8925dcb87b33cc254d3de012a1fd1bb80
JSHC <johan.hansson.carlberg@gmail.com>
2023-04-18 19:09:56 +0200

    Remove unused imports, update snapshots and packages

---
+++ src/components/ShoppingCartLink.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import ShoppingCartLink from './ShoppingCartLink';
+import { MemoryRouter } from 'react-router-dom';
+
+describe('ShoppingCartLink component', () => {
+    it('renders', () => {
+        const { container } = render(
+        <MemoryRouter>
+        <ShoppingCartLink />
+        </MemoryRouter>
+        );
+        expect(container).toMatchSnapshot();
+    }) 
+})
