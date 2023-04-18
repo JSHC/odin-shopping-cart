4a9404813795bf4de3b55cff6d679fa619422c03
JSHC <johan.hansson.carlberg@gmail.com>
2023-04-18 12:29:02 +0200

    Add initial ShopItem controls for quantity

--- src/components/ShopItem.js
+++ src/components/ShopItem.js
@@ -1,9 +1,19 @@
+import { useState } from 'react';
+import '../styles/ShopItem.css';
 const ShopItem = (props) => {
     const { id, name, price } = props;
+
+    const [quantity, setQuantity] = useState(0);
     return (
-        <div>
+        <div className="shop-item">
             <span className="shop-item-name">{name}</span>
             <span className="shop-item-price">{price}</span>
+
+            <div class="shop-item-controls">
+                <input type="text" className='shop-item-quantity' value={quantity}></input>
+                <button>Inc</button>
+                <button>Dec</button>
+            </div>
         </div>
     )
 }
