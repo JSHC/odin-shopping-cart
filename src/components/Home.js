5b85442e3b104f4e695be4d6ed82e5ac50990dbd
JSHC <johan.hansson.carlberg@gmail.com>
2023-04-18 19:08:52 +0200

    Add key to ShopItem map

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,17 @@
+import ShopItem from "./ShopItem";
+
 const Home = () => {
+    const shopItems = [
+        {id: 1, name: 'Item1', price: 10}
+    ]
     return (
         <div>
             <h1>Home</h1>
+            <div className="shop-items-container">
+                {shopItems.map(item => {
+                    return <ShopItem {...item} key={item.id}/>
+                })}
+            </div>
         </div>
     );
 };
