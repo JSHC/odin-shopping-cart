cca7b1bf4fad6b4f10fb4a69eb3204a923ee967e
JSHC <johan.hansson.carlberg@gmail.com>
2023-04-18 10:50:51 +0200

    Wrap header test with MemoryRouter

--- src/components/Header.test.js
+++ src/components/Header.test.js
@@ -2,10 +2,15 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import Header from './Header';
+import { MemoryRouter } from 'react-router-dom';
 
 describe('Header component', () => {
     it('renders', () => {
-        const { container } = render(<Header />);
+        const { container } = render(
+        <MemoryRouter>
+        <Header />
+        </MemoryRouter>
+        );
         expect(container).toMatchSnapshot();
     }) 
 })
