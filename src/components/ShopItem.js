9245727a51a8a8e7a2f0ff461d1f94bf5a653829
JSHC <johan.hansson.carlberg@gmail.com>
2023-04-18 12:41:50 +0200

    Add onChange function for updating quantity in ShopItem

--- src/components/ShopItem.js
+++ src/components/ShopItem.js
@@ -6,19 +6,34 @@ const ShopItem = (props) => {
     const [quantity, setQuantity] = useState(0);
 
     const increaseQuantity = () => {
-        setQuantity(quantity => quantity + 1);
+        setQuantity(quantity => parseInt(quantity + 1));
     }
 
     const decreaseQuantity = () => {
-        setQuantity(quantity => quantity > 0 ? quantity - 1 : 0);
+        setQuantity(quantity => quantity > 0 ? parseInt(quantity - 1) : 0);
+    }
+
+    const updateQuantity = (e) => {
+        if (e.target.value === '') {
+            setQuantity('');
+        } else if (parseInt(e.target.value) === 0) {
+            setQuantity(0);
+        } else {
+            setQuantity(parseInt(e.target.value) ? parseInt(e.target.value) : 0);
+        }
+        
     }
     return (
         <div className="shop-item">
             <span className="shop-item-name">{name}</span>
             <span className="shop-item-price">{price}</span>
 
-            <div class="shop-item-controls">
-                <input type="text" className='shop-item-quantity' value={quantity}></input>
+            <div className="shop-item-controls">
+                <input type="text" 
+                    className='shop-item-quantity' 
+                    value={quantity} 
+                    onChange={updateQuantity}
+                    />
                 <button onClick={increaseQuantity}>Inc</button>
                 <button onClick={decreaseQuantity}>Dec</button>
             </div>
