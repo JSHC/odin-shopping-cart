92356e193d31562658d20b7d8e1865e0d11f2cca
JSHC <johan.hansson.carlberg@gmail.com>
2023-04-19 09:07:47 +0200

    Add function to get quantity of items in shopping cart

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,13 +2,26 @@ import { Link } from 'react-router-dom';
 import '../styles/Header.css';
 import ShoppingCartLink from './ShoppingCartLink';
 
-const Header = () => {
+const Header = (props) => {
+    const {cartItems} = props;
+    const getNumberOfCartItems = () => {
+        if (!cartItems || cartItems.length === 0) {
+            return 0;
+        } else {
+            const totalQuantity = cartItems.map(item => item.quantity).reduce((prev, curr) => {
+            return prev + curr;
+            })
+
+            return totalQuantity;
+        }
+    }
+
     return (
         <div className="header">
             <nav>
                 <ul>
                     <li><Link to='/'>Home</Link></li>
-                    <li><ShoppingCartLink /></li>
+                    <li><ShoppingCartLink cartItemsNumber={getNumberOfCartItems()}/></li>
                 </ul>
             </nav>
         </div>
