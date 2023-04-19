b8cda57dda4bea025014c1d21dd746dcb3b42ac5
JSHC <johan.hansson.carlberg@gmail.com>
2023-04-19 08:18:15 +0200

    Add onAddToCart in Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,19 @@
 import ShopItem from "./ShopItem";
 
-const Home = () => {
+const Home = (props) => {
     const shopItems = [
         {id: 1, name: 'Item1', price: 10}
     ]
+
+    const onAddToCart = (item) => {
+        props.onAddToCart(item);
+    }
     return (
         <div>
             <h1>Home</h1>
             <div className="shop-items-container">
                 {shopItems.map(item => {
-                    return <ShopItem {...item} key={item.id}/>
+                    return <ShopItem {...item} onAddToCart={onAddToCart} key={item.id}/>
                 })}
             </div>
         </div>
