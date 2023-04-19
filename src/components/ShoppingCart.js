b07f68dced93c4caadcc10084a10e0958f3d091c
JSHC <johan.hansson.carlberg@gmail.com>
2023-04-19 10:02:01 +0200

    Add initial implementation of shopping cart item listings in ShoppingCart component

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -1,7 +1,25 @@
+import '../styles/ShoppingCart.css';
+
 const ShoppingCart = (props) => {
+    const {cartItems} = props;
     return (
         <div>
             <h1>Shopping Cart</h1>
+            <div className='shopping-cart-item-header'>
+                <p>Product</p>
+                <p>Quantity</p>
+                <p>Price</p>
+            
+            {cartItems.map(item => {
+                return (
+                    <div className="shopping-cart-item" key={item.id}>
+                        <p>{item.name}</p>
+                        <p>{item.quantity}</p>
+                        <p>{item.price}</p>
+                    </div>
+                )
+            })}
+            </div>
         </div>
     );
 };
