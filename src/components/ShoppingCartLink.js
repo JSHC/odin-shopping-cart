c1aa4d72e29c0107475148d7ac913d239edb9a50
JSHC <johan.hansson.carlberg@gmail.com>
2023-04-19 09:08:34 +0200

    Change ShoppingCartLink to show actual quantity of items in cart

--- src/components/ShoppingCartLink.js
+++ src/components/ShoppingCartLink.js
@@ -5,7 +5,7 @@ import { FaShoppingCart } from 'react-icons/fa'
 const ShoppingCartLink = (props) => {
     return (
         <div className="shopping-cart-link-container">
-            <div className="cart-items-number">0</div>
+            <div className="cart-items-number">{props.cartItemsNumber || 0}</div>
             <Link to='/shopping-cart'><FaShoppingCart /></Link>
         </div>
     )
