108475b6f53aa71aaa65208f8933095f7c094c3e
JSHC <johan.hansson.carlberg@gmail.com>
2023-04-20 21:20:09 +0200

    Add shopping cart sum tests

--- src/components/ShoppingCart.test.js
+++ src/components/ShoppingCart.test.js
@@ -19,7 +19,7 @@ describe('ShoppingCart component', () => {
         screen.getByText('10')
     })
 
-    it('redners multiple items', () => {
+    it('renders multiple items', () => {
         const cartItems = [
             {id: 1, name: 'Item1', quantity: 1, price: 10},
             {id: 2, name: 'Item2', quantity: 2, price: 20}
@@ -33,5 +33,22 @@ describe('ShoppingCart component', () => {
         screen.getByText('Item2');
         screen.getByText('2');
         screen.getByText('20');
+    })
+
+    it ('renders correct sum for single item', () => {
+        const cartItems = [
+            {id: 1, name: 'Item1', quantity: 3, price: 10},
+        ]
+        render(<ShoppingCart cartItems={cartItems}/>)
+        screen.getByText('Sum: 30');
+    })
+
+    it ('renders correct sum for multiple items', () => {
+        const cartItems = [
+            {id: 1, name: 'Item1', quantity: 3, price: 10},
+            {id: 2, name: 'Item2', quantity: 2, price: 20}
+        ]
+        render(<ShoppingCart cartItems={cartItems}/>)
+        screen.getByText('Sum: 70');
     })
 })
