518bc2b7b7fc0e225bcd961f6c0f27c0509d5575
JSHC <johan.hansson.carlberg@gmail.com>
2023-04-20 20:46:48 +0200

    Add mutliple item render test

--- src/components/ShoppingCart.test.js
+++ src/components/ShoppingCart.test.js
@@ -18,4 +18,20 @@ describe('ShoppingCart component', () => {
         screen.getByText('1')
         screen.getByText('10')
     })
+
+    it('redners multiple items', () => {
+        const cartItems = [
+            {id: 1, name: 'Item1', quantity: 1, price: 10},
+            {id: 2, name: 'Item2', quantity: 2, price: 20}
+        ]
+
+        render(<ShoppingCart cartItems={cartItems}/>)
+        screen.getByText('Item1');
+        screen.getByText('1');
+        screen.getByText('10');
+
+        screen.getByText('Item2');
+        screen.getByText('2');
+        screen.getByText('20');
+    })
 })
