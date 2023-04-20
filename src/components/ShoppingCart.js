3681abf95b0be311e7cb91894d5fd84cb38cee6f
JSHC <johan.hansson.carlberg@gmail.com>
2023-04-20 21:20:23 +0200

    Add item sum

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -7,21 +7,23 @@ const ShoppingCart = (props) => {
             <h1>Shopping Cart</h1>
             {cartItems && cartItems.length > 0 ? 
             <div className='shopping-cart-item-header'>
-            <p>Product</p>
-            <p>Quantity</p>
-            <p>Price</p>
+                <p>Product</p>
+                <p>Quantity</p>
+                <p>Price</p>
             
-            {cartItems.map(item => {
-                return (
-                    <div className="shopping-cart-item" key={item.id}>
-                        <p>{item.name}</p>
-                        <p>{item.quantity}</p>
-                        <p>{item.price}</p>
-                    </div>
-                )
-            })}
+                {cartItems.map(item => {
+                    return (
+                        <div className="shopping-cart-item" key={item.id}>
+                            <p>{item.name}</p>
+                            <p>{item.quantity}</p>
+                            <p>{item.price}</p>
+                        </div>
+                    )
+                })}
+                <p>Sum: {cartItems.reduce((prev, curr) => prev + (curr.quantity * curr.price), 0)}</p>
             </div>
-            : null}
+            : null
+            }
             
         </div>
     );
