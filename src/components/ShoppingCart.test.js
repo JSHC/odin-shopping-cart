34be57414399ce3e3ccf67bae017e9f4906edd18
JSHC <johan.hansson.carlberg@gmail.com>
2023-04-24 11:57:55 +0200

    Add delete item button tests

--- src/components/ShoppingCart.test.js
+++ src/components/ShoppingCart.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import ShoppingCart from './ShoppingCart';
+import userEvent from "@testing-library/user-event";
 
 describe('ShoppingCart component', () => {
     it('renders', () => {
@@ -57,5 +58,27 @@ describe('ShoppingCart component', () => {
         const cartItems = [];
         render(<ShoppingCart cartItems={cartItems} />);
         screen.getByText("You haven't added any items to the shopping cart.")
+    })
+
+    it ('renders delete item button', () => {
+        const cartItems = [
+            {id: 1, name: 'Item1', quantity: 3, price: 10}
+        ]
+        render(<ShoppingCart cartItems={cartItems}/>)
+        screen.getByRole('button', {name: 'delete-shopping-cart-item'});
+    })
+
+    it ('can click delete item button', async () => {
+        const cartItems = [
+            {id: 1, name: 'Item1', quantity: 3, price: 10}
+        ]
+        const user = userEvent.setup();
+
+        const deleteItemMock = jest.fn();
+        render(<ShoppingCart cartItems={cartItems} onDeleteItem={deleteItemMock}/>)
+        const deleteItemButton = screen.getByRole('button', {name: 'delete-shopping-cart-item'});
+        await user.click(deleteItemButton);
+
+        expect(deleteItemMock).toBeCalled();
     })
 })
