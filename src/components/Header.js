0fbecad5d39b95af04997d4531102fe029d690c6
JSHC <johan.hansson.carlberg@gmail.com>
2023-04-25 19:13:24 +0200

    Add link to Shop

--- src/components/Header.js
+++ src/components/Header.js
@@ -21,6 +21,7 @@ const Header = (props) => {
             <nav>
                 <ul>
                     <li><Link to='/'>Home</Link></li>
+                    <li><Link to='/shop'>Shop</Link></li>
                     <li><ShoppingCartLink cartItemsNumber={getNumberOfCartItems()}/></li>
                 </ul>
             </nav>
