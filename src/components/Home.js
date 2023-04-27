2eef7b2e149fea0d2bc59b021b8fa334c59f11df
JSHC <johan.hansson.carlberg@gmail.com>
2023-04-27 10:20:06 +0200

    Add homepage content

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,13 @@
+import { Link } from 'react-router-dom';
 import '../styles/Home.css';
 
 const Home = (props) => {
     return (
         <div className="home-container">
-            <h1>Home</h1>
+            <div className='home-content'>
+                <h1>JUST ANOTHER FAKE STORE</h1>
+                <Link to='/shop'>SHOP NOW</Link>
+            </div>
         </div>
     );
 };
