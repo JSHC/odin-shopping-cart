24601da2bc847b12fb97d2664334746fe6fd4a83
JSHC <johan.hansson.carlberg@gmail.com>
2023-04-27 10:27:07 +0200

    Wrap render with MemoryRouter to avoid errors due to Link tags

--- src/components/Home.test.js
+++ src/components/Home.test.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import Home from './Home';
+import { MemoryRouter } from 'react-router-dom';
 
 describe('Home component', () => {
     it('renders', () => {
-        const { container } = render(<Home />);
+        const { container } = render(
+        <MemoryRouter>
+        <Home />
+        </MemoryRouter>
+        );
         expect(container).toMatchSnapshot();
     }) 
 })
