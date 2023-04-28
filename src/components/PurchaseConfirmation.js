4f410804b67f9ba339a3840a7eef5232ea39a542
JSHC <johan.hansson.carlberg@gmail.com>
2023-04-28 19:20:14 +0200

    Add functionality to close purchase confirmation when clicking outside of modal

--- src/components/PurchaseConfirmation.js
+++ src/components/PurchaseConfirmation.js
@@ -1,15 +1,27 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "../styles/PurchaseConfirmation.css";
 
 const PurchaseConfirmation = (props) => {
     const [animate, setAnimate] = useState(false);
-
+    const ref = useRef(null);
+    const onClickOutside = (event) => {
+        if (!ref.current.contains(event.target)) {
+            props.onClose();
+        }
+    }
     useEffect(() => {
         setAnimate(true);
+
+        document.addEventListener('click', onClickOutside, true);
+        return () => {
+            document.removeEventListener('click', onClickOutside, true);
+        }
     });
 
+
+
     return (
-        <div className={`purchase-confirmation ${animate ? "show" : "hidden"}`}>
+        <div ref={ref} className={`purchase-confirmation ${animate ? "show" : "hidden"}`}>
             <h1>Thank you for your purchase!</h1>
             <button
                 onClick={props.onClose}
