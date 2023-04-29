c7f74e863ff6996fa90a695c4f802608b1f7af50
JSHC <johan.hansson.carlberg@gmail.com>
2023-04-29 15:49:05 +0200

    Wrap renders with MemoryRouter to make sure useNavigate works

--- src/components/ShoppingCart.test.js
+++ src/components/ShoppingCart.test.js
@@ -3,17 +3,24 @@ import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import ShoppingCart from "./ShoppingCart";
 import userEvent from "@testing-library/user-event";
+import { MemoryRouter } from "react-router-dom";
 
 describe("ShoppingCart component", () => {
     it("renders", () => {
         const cartItems = [];
-        const { container } = render(<ShoppingCart cartItems={cartItems} />);
+        const {container} = render(
+            <MemoryRouter>
+            <ShoppingCart cartItems={cartItems} />
+            </MemoryRouter>);
         expect(container).toMatchSnapshot();
     });
 
     it("renders shopping cart item", () => {
         const cartItems = [{ id: 1, name: "Item1", quantity: 1, price: 10 }];
-        render(<ShoppingCart cartItems={cartItems} />);
+        render(
+            <MemoryRouter>
+            <ShoppingCart cartItems={cartItems} />
+            </MemoryRouter>);
         screen.getByText("Item1");
         screen.getByText("1");
         screen.getAllByText("10");
@@ -25,7 +32,10 @@ describe("ShoppingCart component", () => {
             { id: 2, name: "Item2", quantity: 2, price: 20 },
         ];
 
-        render(<ShoppingCart cartItems={cartItems} />);
+        render(
+            <MemoryRouter>
+            <ShoppingCart cartItems={cartItems} />
+            </MemoryRouter>);
         screen.getByText("Item1");
         screen.getByText("1");
         screen.getByText("10");
@@ -37,7 +47,10 @@ describe("ShoppingCart component", () => {
 
     it("renders correct sum for single item", () => {
         const cartItems = [{ id: 1, name: "Item1", quantity: 3, price: 10 }];
-        render(<ShoppingCart cartItems={cartItems} />);
+        render(
+            <MemoryRouter>
+            <ShoppingCart cartItems={cartItems} />
+            </MemoryRouter>);
         screen.getByText("30");
     });
 
@@ -46,19 +59,28 @@ describe("ShoppingCart component", () => {
             { id: 1, name: "Item1", quantity: 3, price: 10 },
             { id: 2, name: "Item2", quantity: 2, price: 20 },
         ];
-        render(<ShoppingCart cartItems={cartItems} />);
+        render(
+            <MemoryRouter>
+            <ShoppingCart cartItems={cartItems} />
+            </MemoryRouter>);
         screen.getByText("70");
     });
 
     it("renders message when shopping cart is empty", () => {
         const cartItems = [];
-        render(<ShoppingCart cartItems={cartItems} />);
+        render(
+            <MemoryRouter>
+            <ShoppingCart cartItems={cartItems} />
+            </MemoryRouter>);
         screen.getByText("You haven't added any items to the shopping cart.");
     });
 
     it("renders delete item button", () => {
         const cartItems = [{ id: 1, name: "Item1", quantity: 3, price: 10 }];
-        render(<ShoppingCart cartItems={cartItems} />);
+        render(
+        <MemoryRouter>
+        <ShoppingCart cartItems={cartItems} />
+        </MemoryRouter>);
         screen.getByRole("button", { name: "delete-shopping-cart-item" });
     });
 
@@ -68,7 +90,9 @@ describe("ShoppingCart component", () => {
 
         const deleteItemMock = jest.fn();
         render(
+            <MemoryRouter>
             <ShoppingCart cartItems={cartItems} onDeleteItem={deleteItemMock} />
+            </MemoryRouter>
         );
         const deleteItemButton = screen.getByRole("button", {
             name: "delete-shopping-cart-item",
